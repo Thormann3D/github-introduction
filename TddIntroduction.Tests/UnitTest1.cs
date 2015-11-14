a4a45c4738f9d33057a75b3353212fabf929a095
Martin Weber <martin.weber@sharpco.de>
2015-11-14 12:19:36 +0100

    Import project

---
+++ TddIntroduction.Tests/UnitTest1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TddIntroduction.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void When_Not_3_And_Not_5_Return_Number()
+        {
+            int i = 4;
+
+            FizzBuzz fb = new FizzBuzz();
+
+            string x = fb.Query(i);
+        }
+
+        [TestMethod]
+        public void When_5_Expect_Buzz()
+        {
+            int i = 5;
+
+            FizzBuzz fb = new FizzBuzz();
+
+            string x = fb.Query(i);
+            Assert.AreEqual(x, "Buzz");
+        }
+        [TestMethod]
+        public void When_3_Expect_Fizz()
+        {
+            int i = 3;
+
+            FizzBuzz fb = new FizzBuzz();
+
+            string x = fb.Query(i);
+            Assert.AreEqual(x, "Fizz");
+        }
+    }
+
+    internal class FizzBuzz
+    {
+        internal string Query(int i)
+        {
+            if (i == 3)
+                return "Fizz";
+            if (i == 5)
+                return "Buzz";
+            return "Buh";
+        }
+    }
+}
---
+++ TddIntroduction/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TddIntroduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+
+        //public static void DoFizzBuzz()
+        //{
+        //    for (int i = 1; i <= 100; i++)
+        //    {
+        //        bool fizz = i % 3 == 0;
+        //        bool buzz = i % 5 == 0;
+
+        //        if (fizz && buzz)
+        //            Console.WriteLine("FizzBuzz");
+        //        else if (fizz)
+        //            Console.WriteLine("Fizz");
+        //        else if (buzz)
+        //            Console.WriteLine("Buzz");
+        //        else
+        //            Console.WriteLine(i);
+        //    }
+        //}
+    }
+}
